4f7625923b697e0dece71bff85dca844a448bb1e
Jeff Peterson <jeff@petersonj.com>
2018-01-03 12:42:11 -0700

    Disable cookies banner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,7 @@ export default class App extends React.Component {
       modalType: null,
       modalOpen: false,
       helpOn: false,
-      showCookiesBanner: true
+      showCookiesBanner: false
     };
   }
 
