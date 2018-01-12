ed99512dbd8219a41767707945de8055ce36913b
Jeff Peterson <jeff@petersonj.com>
2018-01-12 10:19:50 -0700

    Fix share link clicking

--- src/store/openUrlHandler.js
+++ src/store/openUrlHandler.js
@@ -1,20 +1,11 @@
 import {ipcRenderer} from 'electron'
-import {getProjectFromStorage} from '../utils/storage'
 import {keyFromShareLink} from '../utils/shareLink'
-import Project from '../records/Project'
 
 export default store => {
   ipcRenderer.on('open-url', (_, url) => {
     const id = keyFromShareLink(url)
-    const project = getProjectFromStorage(localStorage, id)
 
-    if (project) {
-      store.dispatch({type: "SET_PROJECT", project})
-      store.dispatch({type: "SEND_NOTIFICATION", message: "Opened project."})
-    } else {
-      const project = Project({id})
-      store.dispatch({type: "SET_PROJECT", project})
-      store.dispatch({type: "SEND_NOTIFICATION", message: "Opening project."})
-    }
+    store.dispatch({type: 'SEND_NOTIFICATION', message: "Opening project."})
+    store.dispatch({type: 'SHARED_PROJECT_ID_ENTERED', id})
   })
 }
