6ee9aae633aa829a5bbb509080bcb845168fec7f
Jeff Peterson <jeff@yak.sh>
2018-02-06 12:19:50 -0700

    Fix configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,46 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import undoable, { includeAction } from 'redux-undo';
-import { fromJS } from 'immutable';
-import reducer from '../store/reducers/reducer';
+import { createStore, applyMiddleware } from 'redux'
+import reducer from '../store/reducers/reducer'
 
 const configureStore = (devMode) => {
-  let store;
-  if (devMode) {
-    store = createStore(undoable(reducer, {
-      filter: includeAction([
-        'CHANGE_DIMENSIONS',
-        'CLONE_PROJECT',
-        'DRAW_CELL',
-        'SET_PROJECT',
-        'SET_CELL_SIZE',
-        'SET_RESET_GRID',
-        'NEW_PROJECT'
-      ]),
-      debug: true
-    }));
-  } else {
-    // Collects initial state created in the server side
-    const initialState = window.__INITIAL_STATE__;
 
-    /* Make immutable the initial state */
-    initialState.present = fromJS(initialState.present);
-    initialState.past = initialState.past.map(item => fromJS(item));
+  return createStore(
+    reducer
+  )
+}
 
-    store = createStore(undoable(reducer, {
-      filter: includeAction([
-        'CHANGE_DIMENSIONS',
-        'CLONE_PROJECT',
-        'DRAW_CELL',
-        'SET_PROJECT',
-        'SET_CELL_SIZE',
-        'SET_RESET_GRID',
-        'NEW_PROJECT'
-      ]),
-      debug: false
-    }), initialState);
-  }
-
-  return store;
-};
-
-export default configureStore;
+export default configureStore
